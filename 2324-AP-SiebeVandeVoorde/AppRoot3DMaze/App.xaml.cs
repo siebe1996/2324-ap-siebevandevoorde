e9609eee313a2303b6e54cec816918d1a1781825
siebe van de voorde <siebe.vandevoorde@student.odisee.be>
2023-10-17 12:16:37 +0200

    Added new project for 3D maze

---
+++ 2324-AP-SiebeVandeVoorde/AppRoot3DMaze/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer;
+using Globals.Interfaces;
+using LogicLayer;
+using PresentationLayer;
+using System.Windows;
+
+namespace AppRoot3DMaze
+{
+    /// <summary>
+    /// Composition root for App
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IMazeDataAccess data = MazeDataAccessFactory.CreateMazeDataAccess();
+            IBasicMazeGenerator basicMazeGenerator = BasicMazeGeneratorFactory.CreateBasicMazeGenerator(data);
+            IMazeGenerator addWallMazeGenerator = AddWallMazeGeneratorFactory.CreateAddWallMazeGenerator();
+            IMazeGenerator removeWallMazeGenerator = RemoveWallMazeGeneratorFactory.CreateRemoveWallMazeGenerator();
+            new MainWindow3D().Show();
+        }
+    }
+}
---
+++ 2324-AP-SiebeVandeVoorde/PresentationLayer/MainWindow3D.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow3D.xaml
+    /// </summary>
+    public partial class MainWindow3D : Window
+    {
+        public MainWindow3D()
+        {
+            InitializeComponent();
+        }
+    }
+}
